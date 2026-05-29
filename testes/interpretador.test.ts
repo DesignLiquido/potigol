2947dae9aba28dc4887c5a42ad09ada2d5a8c25e
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-05-29 15:19:52 -0700

    Teste unitário com caso reportado em https://github.com/DesignLiquido/potigol/issues/205.

--- testes/interpretador.test.ts
+++ testes/interpretador.test.ts
@@ -400,6 +400,19 @@ describe('Interpretador (Potigol)', () => {
                 expect(_saidas).toHaveLength(1);
                 expect(_saidas[0]).toBe('1');
             });
+
+            it('método em literal de texto (issue #205)', async () => {
+                const retornoLexador = lexador.mapear([
+                    'escreva "abc".tamanho'
+                ], -1);
+
+                const retornoAvaliadorSintatico = await avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoInterpretador = await interpretador.interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                expect(retornoInterpretador.erros).toHaveLength(0);
+                expect(_saidas).toHaveLength(1);
+                expect(_saidas[0]).toBe('3');
+            });
         })
 
         describe('Matrizes', () => {
